5452e3202fc3ce083ad7c6015054142fef670bdc
webmad <alecmei.gubin@yandex.ru>
2021-09-12 10:18:00 +0300

    Добавил название для настройки

--- app/Models/SystemSetting.php
+++ app/Models/SystemSetting.php
@@ -12,7 +12,7 @@ class SystemSetting extends Model
     const START_YEAR_SETTING = 'start_year';
     const COMMUNITY_CHAT_ID_SETTING = 'community_chat_id';
     const VK_TOKEN_SETTING = 'vk_token';
-    const VK_CONFIRMATION_STRING_SETTING = '';
+    const VK_CONFIRMATION_STRING_SETTING = 'vk_confirmation_string';
 
     protected $fillable = ['name', 'value'];
 }
